1c486285993c3d7c29ad0c2ccb83df1332cbfbbb
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-06-24 15:41:30 +0300

    feat(swayfx): Force keybindings
    Override package & config.keybindings with lib.mkForce
    Add comments

--- home/astrumaureus/features/desktop/wayland/swayfx/default.nix
+++ home/astrumaureus/features/desktop/wayland/swayfx/default.nix
@@ -12,7 +12,7 @@
   };
 in {
   imports = [
-    ../../common  # All common
+    ../../common  # Wayland & X11 common
     ../common     # Wayland common
 
     ./i3status.nix
@@ -20,24 +20,82 @@ in {
 
   wayland.windowManager.sway = {
     enable = true;
-    package = pkgs.swayfx;
+    package = lib.mkForce pkgs.swayfx;
 
     config = {
+
+
+      # INFO: Gaps & Title bars
       gaps = {
         inner = 16;
       };
 
       window.titlebar = false;
 
+
+      # INFO: Keybindings
       modifier = "Mod4";
       keybindings = let
         MOD = config.wayland.windowManager.sway.config.modifier;
-        in lib.mkOptionDefault {
+        in lib.mkForce {
           "${MOD}+Return" = "exec ${pkgs.wezterm}/bin/wezterm start --always-new-process";
           "${MOD}+Shift+q" = "kill";
           "${MOD}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+
+          # Shift focus
+          "${MOD}+h" = "focus left";
+          "${MOD}+j" = "focus down";
+          "${MOD}+k" = "focus up";
+          "${MOD}+l" = "focus right";
+
+          # Move window
+          "${MOD}+Shift+h" = "move left";
+          "${MOD}+Shift+j" = "move down";
+          "${MOD}+Shift+k" = "move up";
+          "${MOD}+Shift+l" = "move right";
+
+          # Workspaces
+          "${MOD}+1" = "workspace number 1";
+          "${MOD}+2" = "workspace number 2";
+          "${MOD}+3" = "workspace number 3";
+          "${MOD}+4" = "workspace number 4";
+          "${MOD}+5" = "workspace number 5";
+          "${MOD}+6" = "workspace number 6";
+          "${MOD}+7" = "workspace number 7";
+          "${MOD}+8" = "workspace number 8";
+          "${MOD}+9" = "workspace number 9";
+          "${MOD}+0" = "workspace number 10";
+
+          "${MOD}+Shift+1" = "move container to workspace number 1";
+          "${MOD}+Shift+2" = "move container to workspace number 2";
+          "${MOD}+Shift+3" = "move container to workspace number 3";
+          "${MOD}+Shift+4" = "move container to workspace number 4";
+          "${MOD}+Shift+5" = "move container to workspace number 5";
+          "${MOD}+Shift+6" = "move container to workspace number 6";
+          "${MOD}+Shift+7" = "move container to workspace number 7";
+          "${MOD}+Shift+8" = "move container to workspace number 8";
+          "${MOD}+Shift+9" = "move container to workspace number 9";
+          "${MOD}+Shift+0" = "move container to workspace number 10";
+
+          # Toggle splitting
+          "${MOD}+n" = "split horizontal";
+          "${MOD}+v" = "split vertical";
+
+          # Fullscreen & Floating
+          "${MOD}+F" = "fullscreen toggle";
+          "${MOD}+Shift+F" = "floating toggle";
+          "${MOD}+Shift+Space" = "focus mode_toggle";
+
+          # Modes
+          "${MOD}+R" = "mode resize";
+
+          # Reload & Exit
+          "${MOD}++Shift+R" = "reload";
+          "${MOD}+Ctrl+Shift+E" = "exit";
       };
 
+
+      # INFO: Colors
       colors = {
         focused = {
           background = "#${colors.base00}";
@@ -64,8 +122,8 @@ in {
         };
       };
 
-      fonts = fonts;
 
+      # INFO:: Bar
       bars = [
         {
           position = "top";
@@ -105,6 +163,8 @@ in {
           fonts = fonts;
         }
       ];
+
+      fonts = fonts;
     };
   };
 }
