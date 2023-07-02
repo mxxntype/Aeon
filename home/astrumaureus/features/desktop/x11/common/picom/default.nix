21d54e9995ea99311bf62252bba22c55bf77d797
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-02 07:30:38 +0300

    feat(picom): Add shadows, increase inactiveOpacity
    feat: Enabled shadow
    fix: Set inactiveOpacity = 0.9

--- home/astrumaureus/features/desktop/x11/common/picom/default.nix
+++ home/astrumaureus/features/desktop/x11/common/picom/default.nix
@@ -6,8 +6,19 @@
     enable = true;
     vSync = true;
     fade = true;
+    shadow = true;
     activeOpacity = 1.0;
-    inactiveOpacity = 0.85;
+    inactiveOpacity = 0.9;
+
+    settings = {
+      # WARN: Very laggy
+      /* blur = {
+        method = "gaussian";
+        size = 8;
+        deviation = 8.0;
+      };
+      */
+    };
 
     package = pkgs.picom;             # Default picom
     # package = pkgs.picom-jonaburg;  # ISSUE: Crashes almost instantly
