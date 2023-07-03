99848305dbbf5d594a706ddba502237e267b910b
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-03 22:40:23 +0300

    feat(picom): Add Pijulius' picom with animations

--- home/astrumaureus/features/desktop/x11/common/picom/default.nix
+++ home/astrumaureus/features/desktop/x11/common/picom/default.nix
@@ -1,26 +1,54 @@
+# INFO: Picom, X11 compositor
+
 {
   pkgs,
+  lib,
   ...
 }: {
   services.picom = {
     enable = true;
     vSync = true;
+
     fade = true;
-    shadow = true;
+    shadow = false;
+
     activeOpacity = 1.0;
-    inactiveOpacity = 0.9;
+    inactiveOpacity = 1.0;
 
     settings = {
-      # WARN: Very laggy
-      /* blur = {
-        method = "gaussian";
-        size = 8;
-        deviation = 8.0;
-      };
-      */
+      animations = true;
+
+      animation-stiffness = 300;
+      animation-window-mass = 0.7;
+      animation-dampening = 25;
+      animation-clamping = false;
+
+      animation-for-open-window = "zoom";
+      animation-for-menu-window = "zoom";
+      animation-for-unmap-window = "zoom";
+      animation-for-workspace-switch-in = "zoom";
+      animation-for-workspace-switch-out = "zoom";
+      animation-for-transient-window = "zoom";
+
+      # TODO: Inherit from host configuration
+      frame-rate = 60;
+
+      mark-wmwin-focused = true;
+      mark-ovredir-focused = true;
+      detect-transient = true;
+      detect-client-leader = true;
+      unredir-if-possible = true;
     };
 
-    package = pkgs.picom;             # Default picom
-    # package = pkgs.picom-jonaburg;  # ISSUE: Crashes almost instantly
+     # Pijulius' Picom
+     package = pkgs.picom.overrideAttrs(o: {
+        src = pkgs.fetchFromGitHub {
+          repo = "picom";
+          owner = "pijulius";
+          rev = "982bb43e5d4116f1a37a0bde01c9bda0b88705b9";
+          sha256 = "YiuLScDV9UfgI1MiYRtjgRkJ0VuA1TExATA2nJSJMhM=";
+        };
+    });
   };
 }
+
