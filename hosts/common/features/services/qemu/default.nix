1ccbadf7e8d57d8fa46ef71d93e42d76276547ec
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-04 12:19:55 +0300

    feat(virtualisation): Add QEMU, virt-manager

---
+++ hosts/common/features/services/qemu/default.nix
@@ -0,0 +1,15 @@
+# INFO: KVM & Qemu virtualisation
+
+{
+  pkgs,
+  ...
+}: {
+  virtualisation.libvirtd = {
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    qemu
+    virt-manager
+  ];
+}
