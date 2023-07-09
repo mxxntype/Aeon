529aa76e4f47e31f87c63c4cc4235abd16905723
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-09 12:52:29 +0300

    feat(nox): Enable QEMU & Syncthing

--- hosts/Nox/default.nix
+++ hosts/Nox/default.nix
@@ -3,23 +3,24 @@
 {
   ...
 }: {
-  # NOTE: List of modules that form the host configuration
+  # List of features that form the host configuration
   imports = [
 
     # Users that should be present on the system
     ../common/users/astrumaureus
-
 
     # Optinonal system-level modules
     ../common/features/boot/quiet-boot.nix
     ../common/features/sound/pipewire
     ../common/features/x11.nix
 
+    # Services
+    ../common/features/services/syncthing
+    ../common/features/services/qemu
 
     # Other features
     ../common/features/langs/c
     ../common/features/langs/rust
-
 
     # WARN: Vital stuff
     ../common/global
