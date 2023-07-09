bfd31c52c74559dc48712030e02bc26c30f8fa57
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-09 12:50:28 +0300

    feat: Add Syncthing

---
+++ hosts/common/features/services/syncthing/default.nix
@@ -0,0 +1,48 @@
+# INFO: Syncthing, file synchronization service
+
+{
+  config,
+  ...
+}: {
+  imports = [
+    ./devices.nix
+    ../../../users/astrumaureus # Needed to run as user
+  ];
+
+  services.syncthing = {
+    enable = true;
+
+    user = "astrumaureus";
+    group = "wheel";
+    dataDir = "${config.users.users.astrumaureus.home}";
+    configDir = "${config.users.users.astrumaureus.home}/.config/syncthing";
+
+    overrideDevices = true;
+    overrideFolders = true;
+
+    # TODO: Declare separately & define per-device
+    folders = {
+      obsidian-vault = {
+        label = "Obsidian Vault";
+        path = "${config.users.users.astrumaureus.home}/Obsidian/Vault";
+        devices = [
+          "wisp"
+        ];
+        id = "3d84338a-7a26-4777-a446-67f7f8ba7faa";
+      };
+      shadow = {
+        label = "Shadow";
+        path = "${config.users.users.astrumaureus.home}/Files/.shadow";
+        devices = [
+          "wisp"
+        ];
+        id = "in9ln-y1wl5";
+      };
+    };
+
+    extraOptions = {
+      gui.theme = "black";                    # Save your eyes
+      options.globalAnnounceEnabled = false;  # Disable global discovery to protect IP address & save traffic
+    };
+  };
+}
---
+++ hosts/common/features/services/syncthing/devices.nix
@@ -0,0 +1,15 @@
+# INFO: All known syncthing devices
+
+{
+  ...
+}: {
+  services.syncthing.devices = {
+
+    wisp = {
+      name = "Wisp";
+      introducer = false;
+      id = "S7DQX4P-FEELRVT-JIILMKT-CNRPGFC-6XPKH6P-I3BBB2T-4XFWCGQ-DKH6VQO";
+    };
+
+  };
+}
