4421b008ecfa6b75b979a46f6ab85f2b2dc31dbe
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-13 21:39:58 +0300

    feat: Add `astrumaureus@Wyrm`

---
+++ home/astrumaureus/Wyrm.nix
@@ -0,0 +1,31 @@
+# INFO: astrumaureus@Wyrm configuration
+
+{
+  ...
+}: {
+  imports = [
+    ./global
+
+    # ./features/desktop/wayland/swayfx
+    # ./features/desktop/x11/i3
+    # ./features/desktop/x11/xmonad
+    ./features/desktop/x11/bspwm
+
+    ./features/apps/freetube
+    ./features/apps/gimp
+    ./features/apps/keepassxc
+    ./features/apps/libreoffice
+    ./features/apps/librewolf
+    ./features/apps/obsidian
+    ./features/apps/telegram/kotatogram
+    ./features/apps/upscayl
+    ./features/apps/wezterm
+
+    ./features/music
+
+    ./features/games/minecraft
+  ];
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
