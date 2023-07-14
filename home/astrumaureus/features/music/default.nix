b070be4ff07a9b9b3d3b1eaf2cb2c745eec95b21
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-14 13:00:00 +0300

    fix(music): Configure `audio_output` for `mpd`

--- home/astrumaureus/features/music/default.nix
+++ home/astrumaureus/features/music/default.nix
@@ -6,6 +6,12 @@
   services.mpd = {
     enable = true;
     musicDirectory = "${config.home.homeDirectory}/Music";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "PipeWire Sound Server"
+      }
+    '';
   };
 
   programs.ncmpcpp = {
