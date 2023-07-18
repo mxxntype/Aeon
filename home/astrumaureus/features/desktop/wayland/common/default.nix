ba73a883d4572d85e0fe1d33f90f2b9f982ee242
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-18 16:05:06 +0300

    feat(wayland): Add `wl-clipboard`, import `eww`

--- home/astrumaureus/features/desktop/wayland/common/default.nix
+++ home/astrumaureus/features/desktop/wayland/common/default.nix
@@ -1,10 +1,16 @@
+# INFO: Wayland commons
+
 {
   pkgs,
   ...
 }: {
+  imports = [
+    ./eww
+  ];
+
   home.packages = with pkgs; [
     wlsunset
     swww
-    eww-wayland
+    wl-clipboard
   ];
 }
