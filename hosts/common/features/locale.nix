94e2fbe0779a6b1d82dcd9c0ed2c1b3985f2540e
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-20 06:33:26 +0300

    fix(locale): Change `LC_TIME` to `en_US.UTF-8`
    fix: Set `i18n.extraLocaleSettings.LC_TIME` to `en_US.UTF-8`
    refactor: Add explicit definitions for other locale settings

--- hosts/common/features/locale.nix
+++ hosts/common/features/locale.nix
@@ -5,7 +5,18 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "ru_RU.UTF-8";
+      LC_CTYPE          = lib.mkDefault "en_US.UTF-8";
+      LC_NUMERIC        = lib.mkDefault "en_US.UTF-8";
+      LC_TIME           = lib.mkDefault "en_US.UTF-8";
+      LC_COLLATE        = lib.mkDefault "en_US.UTF-8";
+      LC_MONETARY       = lib.mkDefault "en_US.UTF-8";
+      LC_MESSAGES       = lib.mkDefault "en_US.UTF-8";
+      LC_PAPER          = lib.mkDefault "en_US.UTF-8";
+      LC_NAME           = lib.mkDefault "en_US.UTF-8";
+      LC_ADDRESS        = lib.mkDefault "en_US.UTF-8";
+      LC_TELEPHONE      = lib.mkDefault "en_US.UTF-8";
+      LC_MEASUREMENT    = lib.mkDefault "en_US.UTF-8";
+      LC_IDENTIFICATION = lib.mkDefault "en_US.UTF-8";
     };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
