5376c83b9f70322fb313fca3638771f1bf614f43
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-20 07:54:09 +0300

    fix(x11): Remove unnecessary stuff from `.xinitrc`s

--- home/astrumaureus/features/desktop/x11/xmonad/default.nix
+++ home/astrumaureus/features/desktop/x11/xmonad/default.nix
@@ -29,39 +29,6 @@ in {
   home.file.".xinitrc" = {
     text = ''
       #!/bin/sh
-
-      userresources=$HOME/.Xresources
-      usermodmap=$HOME/.Xmodmap
-      sysresources=/nix/store/i6fwdq3wf24ad5f0hd841nss1qfmh8il-xinit-1.4.1/etc/X11/xinit/.Xresources
-      sysmodmap=/nix/store/i6fwdq3wf24ad5f0hd841nss1qfmh8il-xinit-1.4.1/etc/X11/xinit/.Xmodmap
-
-      # merge in defaults and keymaps
-
-      if [ -f $sysresources ]; then
-          xrdb -merge $sysresources
-      fi
-
-      if [ -f $sysmodmap ]; then
-          xmodmap $sysmodmap
-      fi
-
-      if [ -f "$userresources" ]; then
-          xrdb -merge "$userresources"
-      fi
-
-      if [ -f "$usermodmap" ]; then
-          xmodmap "$usermodmap"
-      fi
-
-      # start some nice programs
-
-      if [ -d /nix/store/i6fwdq3wf24ad5f0hd841nss1qfmh8il-xinit-1.4.1/etc/X11/xinit/xinitrc.d ] ; then
-       for f in /nix/store/i6fwdq3wf24ad5f0hd841nss1qfmh8il-xinit-1.4.1/etc/X11/xinit/xinitrc.d/?*.sh ; do
-        [ -x "$f" ] && . "$f"
-       done
-       unset f
-      fi
-
       trayer  --edge top \
               --align right \
               --expand true \
