b8d88aac3fb85e8ccd6c7b42f21a2e418a79906f
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-22 19:19:25 +0300

    feat(hardware): Add `intel` & `vdpau` packages
    Added packages:
    - `intel-media-driver`
    - `vaapiIntel`
    - `libvdpau-va-gl`

--- hosts/Nox/hardware.nix
+++ hosts/Nox/hardware.nix
@@ -32,7 +32,10 @@
     driSupport = true;
     driSupport32Bit = true;
     extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
       vaapiVdpau
+      libvdpau-va-gl
     ];
   };
   hardware.nvidia = {
