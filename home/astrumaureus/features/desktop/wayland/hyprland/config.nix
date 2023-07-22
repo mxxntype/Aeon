08a8d6ade392b04eabe65f7f97108b24f6d3153a
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-07-22 19:21:01 +0300

    fix(hyprland): Remove `nvidia`-specific `env` calls

--- home/astrumaureus/features/desktop/wayland/hyprland/config.nix
+++ home/astrumaureus/features/desktop/wayland/hyprland/config.nix
@@ -27,11 +27,11 @@ in {
     # TODO: Per-host monitors
     extraConfig = ''
       # --[[ Envvars ]]--
-      env = LIBVA_DRIVER_NAME,nvidia
-      env = XDG_SESSION_TYPE,wayland
-      env = GBM_BACKEND,nvidia-drm
-      env = __GLX_VENDOR_LIBRARY_NAME,nvidia
-      env = WLR_NO_HARDWARE_CURSORS,1
+      # env = LIBVA_DRIVER_NAME,nvidia
+      # env = XDG_SESSION_TYPE,wayland
+      # env = GBM_BACKEND,nvidia-drm
+      # env = __GLX_VENDOR_LIBRARY_NAME,nvidia
+      # env = WLR_NO_HARDWARE_CURSORS,1
 
       ${  # INFO: Monitors
         lib.concatStringsSep "\n" (lib.forEach enabledMonitors (m: ''
