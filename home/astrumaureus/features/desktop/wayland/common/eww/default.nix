3b58345b9fb9b5a208a5bab19d17f7e177c22780
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-08-27 18:21:35 +0300

    feat(eww): Set up basic vertical statusbar for `hyprland`

--- home/astrumaureus/features/desktop/wayland/common/eww/default.nix
+++ home/astrumaureus/features/desktop/wayland/common/eww/default.nix
@@ -7,14 +7,97 @@
 }: let
   inherit (config.colorscheme) colors;
 in {
-  # TODO: Eww via home-manager options
-  /* programs.eww = {
-    enable = true;
-    package = pkgs.eww-wayland;
-    configDir = ./shit;
-  }; */
-
   home.packages = with pkgs; [
     eww-wayland
   ];
+
+  xdg.configFile."eww/eww.yuck".text = let
+    workspaceID = "\${ws.id}";
+    occupiedWorkspacePredicate = "\ws.windows > 0 ? \"occupied\" : \"\"";
+    activeWorkspacePredicate = "\${ws.id == active-workspace ? \"active\" : ${occupiedWorkspacePredicate}}";
+  in ''
+    (deflisten active-workspace "~/.cargo/bin/hyprquery -sq active-workspace")
+    (deflisten active-window "~/.cargo/bin/hyprquery -sq active-window")
+    (deflisten workspaces "~/.cargo/bin/hyprquery -sq workspaces")
+
+    (defwindow statusbar
+      :monitor 0
+      :geometry (geometry
+        :anchor "left center"
+        ; :width "100%"
+        :height "100%"
+      )
+      :stacking "fg"
+      :focusable false
+      :exclusive true
+      (statusbar)
+    )
+
+    (defwidget statusbar []
+      (box
+        :class "statusbar top outer-border"
+        (centerbox
+          :orientation "v"
+          :class "statusbar top container"
+          (box
+            :halign "start"
+            ""
+          )
+          (box
+            :halign "center"
+            (workspaces)
+          )
+          (box
+            :halign "end"
+            ""
+          )
+        )
+      )
+    )
+
+    (defwidget workspaces []
+      (box
+        :class "workspaces top container"
+        :orientation "v"
+        (label
+          :class "workspaces active-numerical"
+          :text active-workspace
+        )
+        (for ws in workspaces
+          (box
+            :class "workspaces entry ${activeWorkspacePredicate}"
+            "${workspaceID}"
+          )
+        )
+      )
+    )
+  '';
+
+  xdg.configFile."eww/eww.scss".text = ''
+    .statusbar {
+      &.top {
+        background: #${colors.base01};
+        color: #${colors.base05};
+
+        &.outer-border {
+          border-right: 4px solid #${colors.base01};
+        }
+
+        &.container {
+          border-right: 2px solid #${colors.base02};
+          padding-right: 4px;
+          padding-left: 4px;
+        }
+      }
+    }
+
+    .workspaces {
+      &.active-numerical { color: #${colors.base02} }
+      &.entry {
+        &.empty { color: #${colors.base03}; }
+        &.occupied { color: #${colors.base05}; }
+        &.active { color: #${colors.base0E}; }
+      }
+    }
+  '';
 }
