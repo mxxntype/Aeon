7b8151709097723b66d527fe979205dcec3099d9
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-08-28 23:15:24 +0300

    feat(home-manager): Add `wm-config` module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,4 +1,5 @@
 {
   monitors = import ./monitors.nix;
   wallpaper = import ./wallpaper.nix;
+  wm-config = import ./wm-config.nix;
 }
---
+++ modules/home-manager/wm-config.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  ...
+}: let
+  inherit (lib) mkOption types;
+in
+{
+  options.wm-config = mkOption {
+    type = types.submodule {
+      options = {
+        # Gaps
+        gaps = mkOption {
+          type = types.submodule {
+            options = {
+              inner = mkOption {
+                type = types.int;
+                default = 8;
+              };
+              outer = mkOption {
+                type = types.int;
+                default = 8;
+              };
+            };
+          };
+        };
+        # Borders
+        border = mkOption {
+          type = types.submodule {
+            options = {
+              thickness = mkOption {
+                type = types.int;
+                default = 2;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
