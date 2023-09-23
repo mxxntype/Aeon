68426d61fb789b6015855e2c04c6deeccc6ce5a2
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-09-23 11:56:02 +0300

    fix(erdtree): Fix `--dir-order` in `inverted` layout

--- home/astrumaureus/features/cli/fish.nix
+++ home/astrumaureus/features/cli/fish.nix
@@ -184,7 +184,7 @@ in {
     # silently enable stuff for certain commands, for example, exa:
     shellAliases = {
       # exa = "exa --icons --sort=type";
-      erd = "erd --layout inverted --human --icons --truncate --dir-order first";
+      erd = "erd --layout inverted --human --icons --truncate --dir-order last";
       erdtree = "echo && erd --suppress-size --follow";
       erdsize = "echo && erd --no-ignore";
     };
