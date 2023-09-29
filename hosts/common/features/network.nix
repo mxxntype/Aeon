c4effc4ace1454d62122a8d0ff830701d54a8c28
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-09-29 21:12:01 +0300

    feat(hosts/network): Add reserved private addresses of other hosts

--- hosts/common/features/network.nix
+++ hosts/common/features/network.nix
@@ -1,6 +1,14 @@
 # INFO: Networking settings
 
 { lib, ... }: {
-  networking.useDHCP = lib.mkDefault true;
-  networking.networkmanager.enable = true;
+  networking = {
+    useDHCP = lib.mkDefault true;
+    networkmanager.enable = true;
+
+    hosts = {
+      # Reserved addresses in home WiFi
+      "192.168.68.101" = [ "Wyrm" "wyrm" ];
+      "192.168.68.111" = [ "Luna" "luna" ];
+    };
+  };
 }
